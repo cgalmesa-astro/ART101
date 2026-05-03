43315012838cb0cb898cfb5921ecc263625633b5
cgalmesa-astro <cgalmesa@ucsc.edu>
2026-05-03 13:49:29 -0700

    lab4

---
+++ environ/env03.js
@@ -0,0 +1,12 @@
+let environmentTitle = "Space Journey";
+
+let environmentElements = ["Jupiter", "Mars", "rocket", "Alien"];
+
+let mainEntity = {
+    name: "Alien guy",
+    type: "alien",
+    mood: "cheeky",
+    isMoving: true,
+    favoriteElement: environmentElements[3]
+};
+
